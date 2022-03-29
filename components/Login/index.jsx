78c32bdbba4c30092a9dd94ff7b58f2fc6af6885
swyssauro <xspynni23@gmail.com>
2022-03-29 14:51:35 -0300

    update page login

--- components/Login/index.jsx
+++ components/Login/index.jsx
@@ -1,113 +1,68 @@
-import React, { useState } from "react";
+import React from "react";
+import { AiFillInstagram, AiOutlineGoogle } from "react-icons/ai";
 import * as S from "./styles";
 
-import { FiArrowRight, FiLogIn, FiArrowLeft } from "react-icons/fi";
-
-import { styled } from "@mui/material/styles";
-import TextField from "@mui/material/TextField";
-
-const Input = styled(TextField)({
-  "& fieldset": {
-    borderColor: "#007FFF",
-  },
-
-  "&:hover fieldset": {
-    borderColor: "#007FFF !important",
-  },
-
-  "& input": {
-    color: "#007FFF",
-    fontFamily: "Poppins",
-  },
-
-  "& input:valid:focus + fieldset": {
-    borderColor: "#007FFF",
-    borderWidth: 1,
-  },
-
-  "& label": {
-    color: "#007FFF",
-    fontFamily: "Poppins",
-  },
-
-  "& label.Mui-focused": {
-    color: "#007FFF",
-  },
-
-  "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
-    borderColor: "#007FFF !important",
-    borderWidth: "1px !important",
-  },
-});
-
 export default function Login() {
-  const [otherMedia, setOtherMedia] = useState(false);
   return (
     <S.Container>
-      <div className="col1">undefined</div>
-      <div className="col2">
-        {otherMedia ? (
-          <S.loginCard>
-            <header>
-              <h3>Login</h3>
-            </header>
-            <S.socialCard background="#007fff">
-              Conecte com o Google <FiLogIn />
-            </S.socialCard>
-            <S.socialCard background="#007fff">
-              Conecte com o Pinterest <FiLogIn />
-            </S.socialCard>
-
-            <S.socialCard background="#007fff">
-              Conecte com o Instagram <FiLogIn />
-            </S.socialCard>
-            <div
-              onClick={() => setOtherMedia(!otherMedia)}
-              className="connects"
-            >
-              <FiArrowLeft /> ou conecte com seu login e senha
-            </div>
-          </S.loginCard>
-        ) : (
-          <S.loginCard>
-            <header>
-              <h3>Login</h3>
-            </header>
+      <div className="background-image">
+        <img className="Imagemtoda" src="" />
+        <section id="tudo1">
+          <main className="principal1">
+            <h2>Login</h2>
             <form action="">
-              <Input
-                label="email"
-                placeholder="email"
-                margin="dense"
-                autoComplete="no-fill"
-                fullWidth
-                focused
-              />
-              <Input
-                type={"password"}
-                label="password"
-                placeholder="password"
-                margin="dense"
-                autoComplete="no-fill"
-                fullWidth
-                focused
-              />
-              <button>
-                <FiLogIn />
-              </button>
-              <div className="infos">
-                <a href="/">esqueci a senha</a>
-                <a href="/">cadasta-se</a>
+              <div className="classes1">
+                <label>Email</label>
+                <input
+                  type="e-mail"
+                  name="username"
+                  placeholder="Coloque seu email"
+                  autoComplete="new-password"
+                  requeired="true"
+                />
+                <div className="underline"></div>
               </div>
-              <div
-                onClick={() => setOtherMedia(!otherMedia)}
-                className="connects"
-              >
-                ou conecte pelas suas midias sociais <FiArrowRight />
+              <div className="classes1">
+                <label> Senha </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Coloque sua senha"
+                />
+                <div className="underline"></div>
               </div>
+
+              <input type="button" value="Login" />
             </form>
-          </S.loginCard>
-        )}
+            <div className="esquecisenha">
+              <a className="Esqs" href="/Esqsenha">
+                Esqueci a Senha
+              </a>
+              <a className="Esqs" href="/cadastro">
+                Cadastra-se
+              </a>
+            </div>
+            <div className="midias">
+              <span>Ou conecte pelas suas midias sociais</span>
+              <div className="social-field facebook">
+                <AiOutlineGoogle color="#fff" size={17} />
+                <a className="Conectar" href="#">
+                  Conecte com o Google
+                </a>
+              </div>
+              <div className="twitter">
+                <div className="social-field twitter">
+                  <AiFillInstagram color="#fff" size={17} />
+                  <a className="Conectar" href="#">
+                    Conecte com o Instagram
+                  </a>
+                </div>
+              </div>
+            </div>
+          </main>
+        </section>
       </div>
+      <div></div>
     </S.Container>
   );
 }
--- components/Login/styles.js
+++ components/Login/styles.js
@@ -1,97 +1,192 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  height: 100vh;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  .col1 {
-    width: 50%;
-    display: grid;
-    place-content: center;
-  }
-
-  .col2 {
-    width: 50%;
-    display: grid;
-    place-content: center;
-  }
-`;
-
-export const Wrapper = styled.div`
-  max-width: 1080px;
-  margin: 0 auto;
-`;
-
-export const socialCard = styled.div`
-  width: 100%;
-  height: 56px;
-  font-size: 1rem;
-  border: 1px solid ${(props) => props.background};
-  font-family: Poppins;
-  color: ${(props) => props.background};
-  border-radius: 4px;
-  cursor: pointer;
-  margin-top: 4px;
-  margin-bottom: 10px;
+.background-image {
+  min-height: 100vh;
+  min-width: 100vw;
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  padding: 0 20px;
-`;
+  justify-content: center;
+}
 
-export const loginCard = styled.div`
+main.principal1 {
+  background: white;
+  min-width: 320px;
+  min-height: 40vh;
+  padding: 2rem;
+  box-shadow: 5px 5px 15px rgb(0 0 0 / 20%);
+  border-radius: 8px;
   width: 400px;
-  height: 100%;
-  border-radius: 5px;
-  padding: 20px;
+  height: 600px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
 
-  header {
-    h3 {
-      margin-bottom: 4px;
-      color: #007fff;
-      font-size: 25px;
-      text-decoration: underline;
-      text-decoration-color: #007fff78;
-    }
-  }
+main h2 {
+  font-weight: 600;
+  position: relative;
+}
 
-  .infos {
-    display: flex;
+main h2::before {
+  content: '';
+  position: absolute;
+  height: 4px;
+  width: 25px;
+  bottom: 3px;
+  left: 0;
+  border-radius: 8px;
+  background: linear-gradient(45deg, #8e2de2, #4a00e0);
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.classes1 {
+  position: relative;
+  margin-bottom: 25px;
+  width: 100%;
+}
+
+form .classes1:first-child {s
+  margin-bottom: 1.5rem;
+}
+
+.classes1 .underline::before {
+  content: '';
+  position: absolute;
+  height: 1px;
+  width: 100%;
+  bottom: -5px;
+  left: 0;
+  background: rgba(0,0,0,0.2);
+}
+
+.esquecisenha {
+  display: flex;
     justify-content: space-between;
-    align-items: center;
-    margin-top: 4px;
-    margin-bottom: 8px;
+}
 
-    a {
-      color: #007fff;
-      text-decoration: none;
-    }
-  }
+.classes1 .underline::after {
+  content: '';
+  position: absolute;
+  height: 1px;
+  width: 100%;
+  bottom: -5px;
+  left: 0;
+  background: linear-gradient(45deg, #8e2de2, #4a00e0);
+  transform: scaleX(0);
+  transition: all .3s ease-in-out;
+  transform-origin: left;
+}
 
-  .connects {
-    color: #007fff;
-    width: 100%;
-    border-top: 1px solid #87c3ff;
-    padding-top: 8px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    cursor: pointer;
-  }
+.classes1 input:focus ~ .underline::after {
+  transform: scaleX(1);
+}
 
-  button {
-    width: 100%;
-    height: 56px;
-    font-size: 1rem;
-    background: #007fff;
-    font-family: Poppins;
-    color: #fff;
-    border-radius: 4px;
-    margin: 4px 0px;
-    line-height: 0;
-    cursor: pointer;
-  }
+.classes1 input {
+  outline: none;
+  font-size: 0.9rem;
+  color: rgba(0, 0, 0, 0.7);
+  width: 100%;
+}
+
+.classes1 input::placeholder {
+  color: rgba(0, 0, 0, 0.5);
+}
+
+form input[type="button"] {
+  margin-top: 1rem;
+  padding: 10px;
+  width: 100%;
+  background: linear-gradient(to left, #333ea8, #5f2cb0);
+  cursor: pointer;
+  color: white;
+  font-size: 0.9rem;
+  font-weight: 300;
+  border-radius: 4px;
+  transition: all 0.3s ease;
+}
+
+form input[type="button"]:hover {
+  letter-spacing: 0.5px;
+  background: linear-gradient(to right, #333ea8, #5f2cb0    );
+}
+
+.midias {
+  display: flex;
+  flex-direction: column;
+}
+
+.midias span {
+  color: rgba(0, 0, 0, 0.7);
+  font-size: 0.8rem;
+  text-align: center;
+}
+
+.midias .social-field {
+  padding: 10px;
+  border-radius: 4px;
+  font-size: 0.85rem;
+  width: 100%;
+  cursor: pointer;
+  margin-top: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.midias .social-field a i {
+  margin: 0 0.5rem;
+  width: 15px;
+}
+
+.midias .social-field a {
+  text-decoration: none;
+  color: white;
+}
+
+.midias .social-field.twitter {
+  background: linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%);
+}
+
+.midias .social-field.facebook {
+  background: #ea4335;
+}
+
+.reflogin {
+  font-size:0.8rem
+}
+.background-image {
+  width: 55%;
+  height: 85%;
+  left: 0;
+  top: 0;
+  position: absolute;
+  background-image: url(hhttps://media.discordapp.net/attachments/902364046483939418/930648288959528991/Image.png?width=1191&height=670)no-repeat;
+  display:flex;
+  margin-right: auto;
+}
+.Imagemtoda {
+  width: 500px;
+  margin-bottom:10px;
+  margin-left:500px
+}
+.Google {
+  color: blue
+}
+.Conectar {
+  margin-left: 20px;
+}
+.Esqs:hover {
+  text-decoration:underline;
+  color:blue;
+}
+.Esqs {
+  font-size:.8rem;
+  display: inline-block;
+  text-decoration: none;
+}
 `;
